96c5e6dba698fb41112ebe8c3ffc4056bdc3dd66
kgabara2 <kgabara2@consdata.pl>
2020-09-11 22:17:30 +0200

    communication with scripts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+      <h1>Connected to {{port.name}}</h1>
+      <h2>Received {{receivedMessage}}</h2>
+      <button (click)="sendMessage()">Send message to content script</button>
+  `,
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'review-common-util-helper';
+  counter = 0;
+  receivedMessage = '';
+  port;
+
+
+  constructor(private ref: ChangeDetectorRef) {
+  }
+
+  ngOnInit(): void {
+    this.port = chrome.runtime.connect({name: 'name'});
+    this.port.onDisconnect.addListener(() => this.receivedMessage = 'Disconnected');
+    this.port.onMessage.addListener((msg) => {
+      this.handleMessage(msg);
+    });
+  }
+
+  sendMessage(): void {
+    this.port.postMessage('Hello there from angular' + this.counter);
+  }
+
+  handleMessage(msg: any): void {
+    this.receivedMessage = msg;
+    this.ref.detectChanges();
+    this.counter++;
+  }
 }
